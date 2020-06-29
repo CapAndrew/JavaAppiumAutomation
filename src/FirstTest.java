b193511bbb82ec40151b3863a8ee9622207374c3
glavatskiy <a.glavatskiy@cft.ru>
2020-06-29 15:04:55 +0700

    Урок 4. Ex5: Тест: сохранение двух статей

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -341,6 +341,105 @@ public class FirstTest {
         );
     }
 
+    @Test
+    public void saveTwoArticleToMyList() throws InterruptedException {
+        searchArticlesAndOpenOne("Java", "Object-oriented programming language");
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc = 'More options']"),
+                "Cannot find button to open article options",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/title'][contains(@text , 'Add to reading list')]"),
+                "Cannot find button to add article to reading list",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Cannot find 'Got it' tip overlay",
+                5
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot find input to set name of articles folder",
+                5
+        );
+
+        String folder_name = "Learning programming";
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot put text into articles folder input",
+                folder_name,
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text = 'OK']"),
+                "Cannot press OK button",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc = 'Navigate up']"),
+                "Cannot find X link",
+                5
+        );
+
+        searchArticlesAndOpenOne("Java", "Island of Indonesia");
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc = 'More options']"),
+                "Cannot find button to open article options",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/title'][contains(@text , 'Add to reading list')]"),
+                "Cannot find button to add article to reading list",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.TextView[@text = '"+folder_name+"']"),
+                "Cannot find existing folder",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc = 'Navigate up']"),
+                "Cannot find X link",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc = 'My lists']"),
+                "Cannot find navigation button to My List",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='" + folder_name + "']"),
+                "Cannot find folder with name " + folder_name,
+                5
+        );
+
+        swipeElementToLeft(
+                By.xpath("//*[@text = 'island of Indonesia']"),
+                "Cannot find saved arrticle"
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@text = 'Java (programming language)']"),
+                "Cannot delete saved arrticle",
+                5
+        );
+    }
+
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_message + "\n");
@@ -444,5 +543,37 @@ public class FirstTest {
             String default_message = "An element '" + by.toString() + "' supposed to be not present";
             throw new AssertionError(default_message + " " + error_message);
         }
+    }
+
+    private void searchArticles(String search_line) {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find search input",
+                search_line,
+                5
+        );
+    }
+
+    private void searchArticlesAndOpenOne(String search_line, String article_name) {
+        searchArticles(search_line);
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text = '"+article_name+"']"),
+                "Cannot find article " + article_name,
+                5
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                25
+        );
+
     }
 }
